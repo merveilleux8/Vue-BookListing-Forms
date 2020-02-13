09c95c7ac98d3a847a5853f272390fad6fa3c74a
Merve Pehlivan <merve.pehlivan@foriba.com>
2020-02-13 11:34:51 +0300

    typo fixed

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,7 +5,7 @@
       <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
     <hr />
-    <h2>Fİltered Books By Ownership</h2>
+    <h2>Filtered Books By Ownership</h2>
     <select v-model="holding">
       <option v-for="filter in filters">{{ filter }}</option>
     </select>
